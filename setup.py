04f1046a488fbef2c7e3985f413d0e916876b4dc
Evan Borgstrom <evan@monitor1.fatbox.ca>
2011-12-24 21:18:25 +0000

    Ver 0.2.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
         name='coreclient',
         packages=['core'],
-        version='0.2',
+        version='0.2.1',
         license='BSD',
         description='Client libraries for accessing a CORE deployment',
         long_description=open('README').read(),
