31b08bb9ac2978d8b7794e59d2c1513df332bb87
Evan Borgstrom <evan@fatbox.ca>
2013-11-30 13:55:54 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
         name='coreclient',
         packages=['core'],
-        version='0.2.1',
+        version='0.2.2',
         license='BSD',
         description='Client libraries for accessing a CORE deployment',
         long_description=open('README').read(),
